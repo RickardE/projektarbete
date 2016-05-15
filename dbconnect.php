cf9043e3f9313e225955cbcdfd2fe9cb73c37ad9
RickardE <RickardE@users.noreply.github.com>
2016-05-15 18:07:26 +0200

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,13 @@
 <?php
+define('DB_SERVER', '#');
+define('DB_USERNAME', '#');
+define('DB_PASSWORD', '#');
+define('DB_DATABASE', '#');
 
-$db_host = ""; 
+
+
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
 
 
 ?>
